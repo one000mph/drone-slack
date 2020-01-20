9e35613bf7d6e55d7d841dd4733025447d5a498f
Heather Young <ten000mph@gmail.com>
2020-01-20 09:41:45 -0800

    update to urfave cli v2

--- main.go
+++ main.go
@@ -20,161 +20,161 @@ func main() {
 	app.Action = run
 	app.Version = fmt.Sprintf("%s+%s", version, build)
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "webhook",
 			Usage:  "slack webhook url",
-			EnvVar: "SLACK_WEBHOOK,PLUGIN_WEBHOOK",
+			EnvVars: []string{"SLACK_WEBHOOK,PLUGIN_WEBHOOK"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "ghtoken",
 			Usage:  "github access token",
-			EnvVar: "GITHUB_ACCESS_TOKEN",
+			EnvVars: []string{"GITHUB_ACCESS_TOKEN"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "ghtoslacks",
 			Usage:  "github_slack_lookup",
-			EnvVar: "GITHUB_SLACK_LOOKUP",
+			EnvVars: []string{"GITHUB_SLACK_LOOKUP"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "channel",
 			Usage:  "slack channel",
-			EnvVar: "PLUGIN_CHANNEL",
+			EnvVars: []string{"PLUGIN_CHANNEL"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "recipient",
 			Usage:  "slack recipient",
-			EnvVar: "PLUGIN_RECIPIENT",
+			EnvVars: []string{"PLUGIN_RECIPIENT"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "username",
 			Usage:  "slack username",
-			EnvVar: "PLUGIN_USERNAME",
+			EnvVars: []string{"PLUGIN_USERNAME"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "template",
 			Usage:  "slack template",
-			EnvVar: "PLUGIN_TEMPLATE",
+			EnvVars: []string{"PLUGIN_TEMPLATE"},
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:   "link-names",
 			Usage:  "slack link names",
-			EnvVar: "PLUGIN_LINK_NAMES",
+			EnvVars: []string{"PLUGIN_LINK_NAMES"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "image",
 			Usage:  "slack image url",
-			EnvVar: "PLUGIN_IMAGE_URL",
+			EnvVars: []string{"PLUGIN_IMAGE_URL"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "icon.url",
 			Usage:  "slack icon url",
-			EnvVar: "PLUGIN_ICON_URL",
+			EnvVars: []string{"PLUGIN_ICON_URL"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "icon.emoji",
 			Usage:  "slack emoji url",
-			EnvVar: "PLUGIN_ICON_EMOJI",
+			EnvVars: []string{"PLUGIN_ICON_EMOJI"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "repo.owner",
 			Usage:  "repository owner",
-			EnvVar: "DRONE_REPO_OWNER",
+			EnvVars: []string{"DRONE_REPO_OWNER"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "repo.name",
 			Usage:  "repository name",
-			EnvVar: "DRONE_REPO_NAME",
+			EnvVars: []string{"DRONE_REPO_NAME"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.sha",
 			Usage:  "git commit sha",
-			EnvVar: "DRONE_COMMIT_SHA",
+			EnvVars: []string{"DRONE_COMMIT_SHA"},
 			Value:  "00000000",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.link",
 			Usage:  "git commit link",
-			EnvVar: "DRONE_COMMIT_LINK",
+			EnvVars: []string{"DRONE_COMMIT_LINK"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.ref",
 			Value:  "refs/heads/master",
 			Usage:  "git commit ref",
-			EnvVar: "DRONE_COMMIT_REF",
+			EnvVars: []string{"DRONE_COMMIT_REF"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.branch",
 			Value:  "master",
 			Usage:  "git commit branch",
-			EnvVar: "DRONE_COMMIT_BRANCH",
+			EnvVars: []string{"DRONE_COMMIT_BRANCH"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.author",
 			Usage:  "git author name",
-			EnvVar: "DRONE_COMMIT_AUTHOR",
+			EnvVars: []string{"DRONE_COMMIT_AUTHOR"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.pull",
 			Usage:  "git pull request",
-			EnvVar: "DRONE_PULL_REQUEST",
+			EnvVars: []string{"DRONE_PULL_REQUEST"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "commit.message",
 			Usage:  "commit message",
-			EnvVar: "DRONE_COMMIT_MESSAGE",
+			EnvVars: []string{"DRONE_COMMIT_MESSAGE"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.event",
 			Value:  "push",
 			Usage:  "build event",
-			EnvVar: "DRONE_BUILD_EVENT",
+			EnvVars: []string{"DRONE_BUILD_EVENT"},
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:   "build.number",
 			Usage:  "build number",
-			EnvVar: "DRONE_BUILD_NUMBER",
+			EnvVars: []string{"DRONE_BUILD_NUMBER"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.status",
 			Usage:  "build status",
 			Value:  "success",
-			EnvVar: "DRONE_BUILD_STATUS",
+			EnvVars: []string{"DRONE_BUILD_STATUS"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.prevstatus",
 			Usage:  "build previous status",
 			Value:  "success",
-			EnvVar: "DRONE_PREV_BUILD_STATUS",
+			EnvVars: []string{"DRONE_PREV_BUILD_STATUS"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.link",
 			Usage:  "build link",
-			EnvVar: "DRONE_BUILD_LINK",
+			EnvVars: []string{"DRONE_BUILD_LINK"},
 		},
-		cli.Int64Flag{
+		&cli.Int64Flag{
 			Name:   "build.started",
 			Usage:  "build started",
-			EnvVar: "DRONE_BUILD_STARTED",
+			EnvVars: []string{"DRONE_BUILD_STARTED"},
 		},
-		cli.Int64Flag{
+		&cli.Int64Flag{
 			Name:   "build.created",
 			Usage:  "build created",
-			EnvVar: "DRONE_BUILD_CREATED",
+			EnvVars: []string{"DRONE_BUILD_CREATED"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.tag",
 			Usage:  "build tag",
-			EnvVar: "DRONE_TAG",
+			EnvVars: []string{"DRONE_TAG"},
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:   "build.deployTo",
 			Usage:  "environment deployed to",
-			EnvVar: "DRONE_DEPLOY_TO",
+			EnvVars: []string{"DRONE_DEPLOY_TO"},
 		},
-		cli.Int64Flag{
+		&cli.Int64Flag{
 			Name:   "job.started",
 			Usage:  "job started",
-			EnvVar: "DRONE_JOB_STARTED",
+			EnvVars: []string{"DRONE_JOB_STARTED"},
 		},
 	}
 
